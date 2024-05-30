4e48edb3ad504932f8db2712aac62ff87dc7e4ba
Taeksha <156405979+Taeksha@users.noreply.github.com>
2024-05-30 12:38:51 +0530

    Add files via upload

---
+++ pattern/06.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+		printf("\t*\n");
+		printf("       * *\n");
+		printf("      *   *\n");
+		printf("     *\t   *\n");
+		printf("    *\t    *\n");
+		printf("   * * * * * *");
+		
+		
+	}
